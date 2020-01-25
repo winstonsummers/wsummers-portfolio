1794fb3584d802f59817108f3c4e65fc4c1aed7f
Winston Summers <wsummers@zulily.com>
2020-01-24 21:15:06 -0800

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 function App() {
   return (
-   <div>
-     <h1>Welcome to my website</h1>
-     <p>It is one minute old today</p>
-   </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          {renderMain()}
+        </Route>
+        <Route >
+          {renderError()}
+        </Route>
+      </Switch>
+    </Router>
   );
+}
+
+const renderError = () => {
+  return (
+    <div>
+      <h1>Well this is akward...</h1>
+    </div>
+  )
+}
+
+const renderMain = () => {
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+      <p>It is one minute old today</p>
+    </div>
+  )
 }
 
 export default App;
