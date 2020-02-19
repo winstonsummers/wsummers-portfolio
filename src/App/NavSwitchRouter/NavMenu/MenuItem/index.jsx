217a3bad3560bd66de0f3637005c8a74861d8a6d
Winston Summers <wsummers@zulily.com>
2020-02-19 12:12:08 -0800

    stubbed out some stuff

--- src/App/NavSwitchRouter/NavMenu/MenuItem/index.jsx
+++ src/App/NavSwitchRouter/NavMenu/MenuItem/index.jsx
@@ -1,14 +1,18 @@
 import './index.module.css';
 import propsInterface from './propsInterface';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 // This is the main function. Checkout the propsInterface file to learn
 // more about what's going on here, the rest is pretty standard react :)
 function main(props = propsInterface) {
   return (
-    <div>
-      <h1>New Compoonet</h1>
-    </div>
+    <NavLink 
+      to={props.path} 
+      activeClassName="selected"
+      >
+      {props.label}
+    </NavLink>
   );
 }
 
--- src/App/NavSwitchRouter/NavMenu/index.jsx
+++ src/App/NavSwitchRouter/NavMenu/index.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 import propsInterface from './propsInterface';
 import MenuItem from './MenuItem';
 
-// This is the main function. Checkout the propsInterface file to learn
+// This is the main function. Checkout propsInterface.js to learn
 // more about what's going on here, the rest is pretty standard react :)
 function main(props = propsInterface) {
+  const content = menuItemRenderer(props);
   return (
-    <div>
-      <h1>New Componet</h1>
-    </div>
+    <nav>
+      {
+        content  // i love this syntax, it's so unnecessarily prolific
+      }
+    </nav>
   );
 }
 
-
+function menuItemRenderer({menuProps}) {
+  if(!Array.isArray(menuProps)) { menuProps = menuProps.menuProps }
+  return menuProps.map(props => <MenuItem {...props} key={props.label}/>)
+}
 
 export default main;
